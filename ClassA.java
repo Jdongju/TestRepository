b1d371cc37a96a32975f95d71b60a60f085f3a0f
Jdongju <dj9110@naver.com>
2017-04-24 14:08:26 +0900

    연습파일 삭제

--- ClassA.java
+++
@@ -1,9 +0,0 @@
-public class ClassA {
-  public static void main(String[] args){
-
-    System.out.println("user1작성");
-
-    System.out.println("User2가 작성");
-
-  }
-}
--- ClassB.java
+++
@@ -1,5 +0,0 @@
-public class ClassB {
-  public static void main(String[] args){
-    System.out.println("User2가 작성");
-  }
-}
