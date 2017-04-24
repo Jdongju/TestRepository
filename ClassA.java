0025d12a7c375180222c70642452d7267ccb49b8
Jdongju <dj9110@naver.com>
2017-04-24 12:18:59 +0900

    수정함

--- ClassA.java
+++ ClassA.java
@@ -1,5 +1,7 @@
 public class ClassA {
   public static void main(String[] args){
-    System.out.println("파일생성");
+    System.out.println("User1이 파일생성");
+      System.out.println("User2가 파일생성");
+      System.out.println("User2가 파일생성2");
   }
 }
