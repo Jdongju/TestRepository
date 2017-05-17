4a039d4eb9808abbe7918a39b357ae9953531c1b
Jdongju <dj9110@naver.com>
2017-05-17 12:48:01 +0900

    17일 Servlet, Spring Servlet in Eclipse

---
+++ JavaWebProgramming/src/listener/ContextLoaderListener.java
@@ -0,0 +1,19 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextLoaderListener implements ServletContextListener { //context(App식별자)가 deploy될때를 감지하다가 deploy될때 메소드를 실행한다.
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		System.out.println("ContextLoaderListener contextInitialized() 실행");
+		String key1=event.getServletContext().getInitParameter("key1"); //웹App의 초기 파라미터값을 얻는다.
+		System.out.println(key1);
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		System.out.println("ContextLoaderListener contextDestroyed() 실행");
+	}
+	
+}
